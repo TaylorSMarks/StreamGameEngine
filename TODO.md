ce68bfd0e8b386e9ad848ebc9833271f6ae57516
Taylor <taylor@marksfam.com>
2024-01-17 23:05:22 -0500

    Starting to add backend for having a Ui.

---
+++ TODO.md
@@ -0,0 +1,15 @@
+1. Implement addText, changeText, addCountdown, and changeCountdown in the javascript side.
+2. Track score, lives, and time on the Kotlin side.
+3. When the countdown ends, decrement lives.
+4. Buttons to join and start games...?
+5. Meshes, not just sphere
+    * Heart (Red)
+    * Star (Yellow)
+    * Cresent Moon (Purple)
+    * Lightning Bolt (Orange)
+    * Snowflake (White)
+    * Rupee (Green)
+    * Raindrop (Blue)
+6. Notions of players and picking random players.
+7. Position the camera appropriately and disallow moving it.
+8. Also fix the damn issue where it keeps crashing based on the SSE Event streams being resized or whatever.
---
+++ src/main/kotlin/com/marksfam/Ui.kt
@@ -0,0 +1,56 @@
+package com.marksfam
+
+import kotlinx.datetime.Instant
+
+
+data class ScreenPosition(val x: Float, val y: Float)
+
+class Text(position: ScreenPosition, text: String) {
+    private val id = idGenerator.incrementAndGet()
+
+    init {
+        controller.emitEvent("addText",
+            "x" to position.x,
+            "y" to position.y,
+            "text" to text,
+            "id" to id)
+    }
+
+    var text = text
+        set(newText) {
+            field = newText
+            controller.emitEvent("changeText",
+                "id" to id,
+                "text" to text)
+        }
+}
+
+class Countdown(position: ScreenPosition, prefix: String, endAt: Instant) {
+    private val id = idGenerator.incrementAndGet()
+
+    init {
+        controller.emitEvent("addCountdown",
+            "x" to position.x,
+            "y" to position.y,
+            "prefix" to prefix,
+            "endAt" to endAt.toEpochMilliseconds(),
+            "id" to id)
+    }
+
+    private fun changed() {
+        controller.emitEvent("changeCountdown",
+            "id" to id,
+            "prefix" to prefix,
+            "endAt" to endAt.toEpochMilliseconds())
+    }
+
+    var prefix = prefix
+        set(newPrefix) {
+            field = newPrefix
+        }
+
+    var endAt = endAt
+        set(newEndAt) {
+            field = newEndAt
+        }
+}
